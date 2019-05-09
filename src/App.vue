a2fe0f0bcd7c08a138e8faee6b061ee4629277c1
juliwolf <qwert0038@yandex.ru>
2019-05-09 11:50:10 +0300

    Updated php lesson

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,6 @@
     <app-header></app-header>
     <div class="container-fluid">
       <div class="row">
-        <!--<div class="col-md-2">-->
-          <!--<app-left-panel></app-left-panel>-->
-        <!--</div>-->
         <div class="col-md-12">
           <router-view></router-view>
         </div>
@@ -17,13 +14,11 @@
 
 <script>
   import Header from './components/addPanels/header.vue'
-  // import leftPanel from './components/addPanels/leftPanel.vue'
 
   export default {
     name: 'app',
     components: {
-      'app-header': Header,
-      // 'app-left-panel': leftPanel
+      'app-header': Header
     }
   }
 </script>
---
+++ src/components/addPanels/footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer id="footer">
+        <p>Copyright by JuliWolf</p>
+    </footer>
+</template>
+
+<style scoped lang="scss">
+    $yellow: #ffe300;
+    $darkBlue: #1c1e22;
+    #footer{
+        background-color:$darkBlue;
+        color:$yellow;
+        font-size: 14px;
+        text-align:center;
+        padding:5px 5px 5px 5px;
+    }
+</style>
